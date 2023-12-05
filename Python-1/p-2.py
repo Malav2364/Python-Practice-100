ba91cf76a05d552f6e88f1593228ec8e36a5f2a1
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-05 22:33:14 +0530

    escape seq and comments

---
+++ Python-1/p-2.py
@@ -0,0 +1,17 @@
+# print("Jay Shree Krishna !!!")
+# print("My name is Malav\npersuing B.tech in CSE") #use \n  to print in a new line 
+# ofc "#" is used for comments 😂😂
+
+"""
+This is a multi line comment 😂
+"""
+
+# this is a escape sequence character
+# print("My name is Malav Patel and 
+#       i am living in gujarat")         this is an invalid syntax as python goes line by line so by going
+#to next line it will consider it as an error
+
+print("My name is Malav Patel \nand i am living in gujarat es-1") #escape sequence character-1 
+print("My name is Malav Patel \tand i am living in gujarat es-2") #escape sequence character-2 for a tab space 
+print("My name is Malav Patel  and i am living in gujarat es-3 //") #escape sequence character-3 for a literlal backslash in the o/p line
+print("This is a single quote : \' and this is a double quote: \" es-4") #escape sequence character-4 for single quote and double quote
