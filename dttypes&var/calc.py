f1a06314637950814094fb21ff981906184f3d29
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-06 12:29:25 +0530

    calc and operators

---
+++ dttypes&var/calc.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+print("Addition : ", a+b)
+print("Subtraction : ", a-b)
+print("Multiplication : ", a*b)
+print("Divison : ", a/b)
+print("Floor Divison : ", a//b)
+print("Modulo : ", a%b)
---
+++ dttypes&var/p-4.py
@@ -0,0 +1,9 @@
+#operators in python
+
+print(16+6)    #addition
+print(16-6)     #subtraction
+print(16*6)     #multiplicaion
+print(16/6)     #division
+print(16//6)    #floor division used to get o/p as a whole number
+print(16%6)    #modulo for reminder of a divison
+print(5**3)      #exponential operator that is 5*3*3*3  
