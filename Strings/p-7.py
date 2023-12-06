e85537be642c57c6f52d7f8e54818a9ef340eaeb
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-06 16:19:45 +0530

    strings and a basic for loop

---
+++ Strings/p-7.py
@@ -0,0 +1,14 @@
+name = "Bhupendra Jogi"
+anotherFriend = "Jay Shree Krishna"
+
+# print("Hello,", name,"\n","\"",anotherFriend,"\"")
+str1 = """lorem ipsum fsjoughadf;gadfghdghadtghaetrgargafgjhadflfgjkafghaalkjfghaflgajfghagafjkghalgajfghlagjkablgfgj""" #we can print anything in 3 double quotes
+# print(str1)
+
+# print(name[0]) #used to print specific part of a strting
+# print(name.upper())
+
+#for loop use for printing the  name letter by letter
+
+for char in name:
+    print(char)
