8bd1687ad487accc9eaa811f2e5e049fd4c887d6
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-06 15:27:43 +0530

    user inputs

---
+++ userinputs/p-6.py
@@ -0,0 +1,7 @@
+# user_inp = input()
+# print("Type of", user_inp, "is", type(user_inp))
+
+#general issue
+x = input("Enter number 1 : ")
+y = input("Enter number 2 : ")
+print("Addition is :", int(x)+ int(y)) #we should perform type casting in it
