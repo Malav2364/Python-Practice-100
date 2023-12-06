ee5e46e124fd4924ffe684e1a2735e03d1789b8f
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-06 11:59:05 +0530

    datatypes in python

---
+++ dttypes&var/p-3.py
@@ -0,0 +1,47 @@
+a = 14633473737433 #integer dataype here a is a variable
+b = "Malav" #string datatype
+c = True #boolean data type 
+d = None #none type 
+# print(a, b, c)
+
+#print(a + b) not possible
+#we need to have same datatype  to perfrom concatination
+
+e = "Malav"
+f = "Patel"
+g = 1.25 #float
+h = complex(2, 3) #complex number #o/p 2+3j
+print(e +" "+ f) #here we can perform concatination
+
+#type function is used to retrive the type of datatype
+# print("The type of a  is ", type(a)) 
+# print("The type of b  is ", type(b)) 
+# print("The type of c  is ", type(c)) 
+# print("The type of d  is ", type(d)) 
+# print("The type of e  is ", type(e)) 
+# print("The type of f  is ", type(f)) 
+# print("The type of g  is ", type(g)) 
+# print("The type of h  is ", type(h)) 
+# print(h)
+
+#lists
+
+list1 = [1, 2, 3, 4, 5, ["Apple", "Banana", "Orange"]]
+# print(list1)
+
+#tuple (immutatble dtypes or cannot be changed)
+
+tuple1 = (("Parrot", "Sparrow"), ("Lion", "Tiger"))
+# print(tuple1)
+
+#dictionary (collection of key value pairs )
+dict1 = {
+    "name" : "Malav",
+    "Marks" : "Full Marks 🤣🤣",
+    "age" : 19,
+    "canVote" : True
+}
+
+print(dict1)
+
+#everything in python is an object 
