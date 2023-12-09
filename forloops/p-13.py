fd53bb9e8c6a5dd94a1ebbfe5f1cd022ddef6133
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-09 22:34:03 +0530

    for loops

---
+++ forloops/p-13.py
@@ -0,0 +1,25 @@
+#loops in python is used for multiple iterations
+#for strings
+name = "Malav"
+# for i in name:
+#     print(i)
+#     if (i == "a"):
+#         print("Pakda gaya !!")
+
+#for lists
+colors = ["Blue", "Black", "Pink", "Red"]
+# for color in colors:
+#     print(color)
+#     for i in color:                         #nested for further desection
+#         print(i)
+
+for m in range(2):  #range function
+    print(m+1)
+
+#iterations of range
+for a in range(1, 2): #for giving range between two numbers
+    print(a+1)
+#itteration with steps in loop
+
+for b in range(1,30,2):
+    print("B",b)      #step paramater in range function used to give gap bettween two numbers
