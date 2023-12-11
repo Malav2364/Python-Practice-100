4038641751beade8da5dde963b0dbbecd4d1b493
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-11 19:13:12 +0530

    while loop in python

---
+++ WhileLoop/p-13.py
@@ -0,0 +1,47 @@
+# for i in range(10):
+#     print(i+i)
+
+#using while loop
+# i = 0
+# while (i<=36): #conditon
+#     i = int(input("Enter the Number :"))
+#     print(i)
+# print("Satisfied the condition!!")
+
+# print("Incrementing loop")
+# i = 0
+# while (i<=10):
+#     i = i+1
+#     print(i*i)
+
+# print("Decrementing While Loop")
+# count = 10
+# while (count > 0):
+#     print(count)
+#     count = count-1
+
+# #while loop using else 
+# count = 10
+# while (count > 0):
+#     print(count)
+#     count = count-1
+# else:
+#     print("Udi baba tum to else condition me aati!")
+
+#no do while loop in python
+#emulate do while loop
+
+# Emulating do-while loop in Python
+
+# Initialize a variable
+
+
+# uipt = input("Enter your name")
+
+# while True:
+#     print("Atleast One time it will be executed!")
+
+#     uspt2 = str(input("yes or no ?"))
+
+#     if uspt2.lower() != "yes":
+#         break
--- forloops/loopprac.py
+++ forloops/loopprac.py
@@ -1,16 +1,34 @@
-#python program to print table in python
-num = 2
-for i in range(1, 11):
-    print(num,'*',i,'=',num*i)
+# #python program to print table in python
+# num = 2
+# for i in range(1, 11):
+#     print(num,'*',i,'=',num*i)
 
-#python program to print square of numbers
-for i in range(1,6):
-    print("Square of",i,"is",i*i)
+# #python program to print square of numbers
+# for i in range(1,6):
+#     print("Square of",i,"is",i*i)
 
-#python program to find factorial of a number
+# #python program to find factorial of a number
 
-number = int(input("Enter a number :"))
-fact = 1
-for i in range(1, number+1):
-    fact*=i
-print(fact)
+# number = int(input("Enter a number :"))
+# fact = 1
+# for i in range(1, number+1):
+#     fact*=i
+# print(fact)
+
+# num= 2
+# for i in range(1,11):
+#     print(num,'*',i,'=',num*i)
+
+# #square
+# for i in range(1,6):
+#     print("The square of",i,"is :",i*i)
+
+#trying star pattern 
+# symbol = '*'
+# for i in range(1,11):
+#     print(symbol, symbol, symbol, symbol)
+
+#triangle using *
+#logic build for traingle 
+#first loop is created for no. of rows in a traingle 
+#then
