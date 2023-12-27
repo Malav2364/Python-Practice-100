a13f3d8ae1155fcaf0016d4649a002e92fbd4bd5
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-27 22:34:54 +0530

    immutable tuples

--- Tuples/p-18.py
+++ Tuples/p-18.py
@@ -1,17 +1,22 @@
 #tuples
-tup1 = (1,2,3,4)
-print(type(tup1))
-print(tup1[0])
-print(tup1[1])  #indexing in tuple 
-print(tup1[2])
-print(tup1.index(2)) #for finding the index in tuple 
-print(tup1.count(1))
+# tup1 = (1,2,3,4)
+# print(type(tup1))
+# print(tup1[0])
+# print(tup1[1])  #indexing in tuple 
+# print(tup1[2])
+# print(tup1.index(2)) #for finding the index in tuple 
+# print(tup1.count(1))
+#to manipulate tuple we need to perform the following steps
+tuple1 = (0,1,1,2,2,3,3,4,2,1,6)
+l1 = list(tuple1) #conversoin of tuple into list
+# print(l1)
+l1.append(5)
+l1.pop(0)
+tuple1 = tuple(l1)
+# print(tuple1)
 
-if 3 in tup1:
-    print("It is in the tuple !!")
+tpl1 = (0,1,2,3,2,3,1,3,2,3)
+# res = tpl1.count(1)
+res = tpl1.index(3,4,8)
+print(res)
 
-
-#for new tuple 
-tup2 = tup1[1:3]
-print(tup2)
-#slicing in tuple 
