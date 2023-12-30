6bc9006b2bdd71a10d7410026134e5b03c67d3b3
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2023-12-30 19:39:52 +0530

    doc strings in python

---
+++ docstring/p-20.py
@@ -0,0 +1,13 @@
+#lets start about doc strings
+
+# '''This is a Docstring in python'''
+
+'''This is a function that returns a square of a number
+And the doc string should be above not below the function or program
+'''
+def square(n):
+    print(n*n)
+square(5)
+print(__doc__)
+
+#write import this to see a poem in python it is a easter egg (ZEN of python)
