615f62e6ab69734207d415ed211b9dff80cf90c4
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-01 15:23:35 +0530

    indexing and slicing is almost done

--- Libs/arrays2.py
+++ Libs/arrays2.py
@@ -1,3 +1,5 @@
+'''This is a vertical stack of the array'''
+
 import numpy as np
 
 ar1 = np.array([1,2,3,4,5,6,7,8,9,10])
@@ -20,13 +22,48 @@ d = np.hstack((ar3, ar4))#stack both array in vertical manner
 
 #reshaping the arange array directly 
 e = np.arange(1,25).reshape(2,12)
-print(e)
+# print(e)
 
 #splitting the array in equal parts
 f = np.hsplit(e, 3)
-print(f)
+# print(f)
 
 #splitting the array from specific index
 
 g = np.hsplit(e,(3,4))
-print(g)
+# print(g)
+
+k = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+m = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+# print(k)
+
+#stacking the arrays horizontally and vertically using vsplit and hsplit
+l = np.vstack((k,m))
+# print(__doc__,l)
+n = np.hstack((k,m))
+# print(n)
+
+#create and reshape the array at same time
+ms = np.arange(1,25).reshape(2,12)
+# print("This is an array directly reshaped when created")
+# print(ms)
+
+#now if we want to split the array in to hsplit or vslpit wecan do this 
+
+aa = np.hsplit(ms, 2)
+# print(aa)
+
+mm = np.vsplit(ms, 2)
+# print(mm)
+
+#crete an array and slice it and modify the first element of the array 
+ca = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+#slicing
+b1 = ca[0, :]
+# print(b1)
+b1[0] = 100
+print(ca)
+
+#copying the array 
+b2 = ca.copy()
+print(b2)
