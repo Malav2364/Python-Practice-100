417458e86a447cebfe2de96f0d1e7be509658f19
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-01 18:46:32 +0530

    intro to sets

---
+++ sets/p22.py
@@ -0,0 +1,17 @@
+#sets in python no repeated entries in sets 
+#unordered collection of data in sets 
+#cannot change values in sets once inserted and executed
+
+gft_data = {2,2,4,5,6,8,8,9}
+# print(gft_data)
+
+#prac
+malav = {}
+print(type(malav)) #this will return that the type is dictionary
+
+#to solve it we need to assign it as a set
+ms1 = set()
+print(type(ms1)) #this will print the type as a set
+
+for i in gft_data:
+    print(i)
