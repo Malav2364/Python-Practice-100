dbb3e165cdfb49f24b0a484ba6f3f3acacec29c2
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-08 19:04:50 +0530

    other operations in arrays using broadcasting

---
+++ Libs/othArrOp.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+m = np.array([[1,2,3], [4,5,6]])
+ #to print max element in the array
+print(m.max())
+ #to print min element in the array
+print(m.min())
+ #to print sum element in the array
+print(m.sum(axis=1))
+
+
