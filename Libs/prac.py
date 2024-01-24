06d221f92d9bdd39c97d6245b65338516f5b5b56
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-24 15:52:41 +0530

    practice of random array is done

--- Libs/prac.py
+++ Libs/prac.py
@@ -3,6 +3,8 @@ import numpy as np
 ra1 = np.random.default_rng()
 randomInt = ra1.integers(1,11, size=((2,2)))
 
-randomInt, ix_count, ind_count = np.unique(randomInt, return_counts=True, return_index=True)
+randomInt, ix_count, ind_count, inv_arr = np.unique(randomInt, return_counts=True, return_index=True, return_inverse=True)
 print(ix_count, "Count of the elements in the array")
 print(ind_count, "Index of the random Array")
+print(inv_arr, "Inverse Array Returned")
+#practice of random array is done ✅
