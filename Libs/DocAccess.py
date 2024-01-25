2b933707049f67bf120f01b8454b49b37add83fd
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-01-25 11:22:09 +0530

    docAccess

---
+++ Libs/DocAccess.py
@@ -0,0 +1,10 @@
+import math
+import numpy as np
+
+
+# help(np.arange) #to get the information for the particular syntax
+
+a = np.array([1,2,3,4,5,6,7,8,9,10])
+
+help(a)
+#help is used to access the documentation of a particular function or operation 
