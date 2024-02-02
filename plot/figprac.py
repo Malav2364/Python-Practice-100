09b333efd03445b9294a2dfa1872352f4b98c0a8
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-02-02 14:27:25 +0530

    grids in graphs

--- plot/figprac.py
+++ plot/figprac.py
@@ -6,7 +6,7 @@ fig, axes = plt.subplots()
 axes.plot([1,2,3], [4,5,6])
 axes.set_xlabel('Same to Same')
 axes.set_ylabel('Same to Same')
-
+plt.grid(True)
 #example of axis
 
 fig, ms = plt.subplots()
@@ -14,4 +14,5 @@ ms.plot([1,2,3], [4,5,6])
 #set labels for each axis 
 ms.set_xlabel('X-axis')
 ms.set_ylabel('Y-axis')
+plt.grid(True)
 plt.show()
