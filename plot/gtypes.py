a4bb26c981a0b310bca63ada341d31d6a285e612
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2024-02-02 17:15:05 +0530

    edgecolor and linewidth

--- plot/gtypes.py
+++ plot/gtypes.py
@@ -8,7 +8,7 @@ c = ['y', 'g', 'b', 'black', 'magenta', 'pink']
 plt.title('Language Popularity from 100')
 plt.xlabel('Languages', fontsize=15)
 plt.ylabel('Popularity', fontsize=15)
-plt.bar(x,y, width=0.5, color=c, align='center') #width is used to give width to the bars in the  graph  and align for the alignment of the bars on the labels in x axis
+plt.bar(x,y, width=0.5, color=c, align='center', linewidth=5, edgecolor="r") #width is used to give width to the bars in the  graph  and align for the alignment of the bars on the labels in x axis
 plt.grid(False)
 plt.minorticks_on()
 
